fe1599b91bb53e0abd1337aa967c8bbab03dbb69
Patryk Robakowski <patryk711@outlook.com>
2018-10-26 09:56:53 +0200

    Created server class with fields and empty methods

---
+++ src/main/java/Server.java
@@ -0,0 +1,98 @@
+import java.util.List;
+
+public class Server {
+
+    // Fields
+    private int round;
+    private Player[2];
+    private String command;
+    private int turn;
+    private List<BasicCard> playerADeck;
+    private List<BasicCard> playerBDeck;
+    private List<List> playerDecks;
+    private List<BasicCard> playerAGraveyard;
+    private List<BasicCard> playerBGraveyard;
+    private List<List> playerGraveyards;
+
+    // Getters & Setters
+    public int getRound() { return round; }
+    public void setRound(int round) { this.round = round; }
+    public String getCommand() { return command; }
+    public void setCommand(String command) { this.command = command; }
+    public int getTurn() { return turn; }
+    public void setTurn(int turn) { this.turn = turn; }
+    public List<BasicCard> getPlayerADeck() { return playerADeck; }
+    public void setPlayerADeck(List<BasicCard> playerADeck) { this.playerADeck = playerADeck; }
+    public List<BasicCard> getPlayerBDeck() { return playerBDeck; }
+    public void setPlayerBDeck(List<BasicCard> playerBDeck) { this.playerBDeck = playerBDeck; }
+    public List<List> getPlayerDecks() { return playerDecks; }
+    public void setPlayerDecks(List<List> playerDecks) { this.playerDecks = playerDecks; }
+    public List<BasicCard> getPlayerAGraveyard() { return playerAGraveyard; }
+    public void setPlayerAGraveyard(List<BasicCard> playerAGraveyard) { this.playerAGraveyard = playerAGraveyard; }
+    public List<BasicCard> getPlayerBGraveyard() { return playerBGraveyard; }
+    public void setPlayerBGraveyard(List<BasicCard> playerBGraveyard) { this.playerBGraveyard = playerBGraveyard; }
+    public List<List> getPlayerGraveyards() { return playerGraveyards; }
+    public void setPlayerGraveyards(List<List> playerGraveyards) { this.playerGraveyards = playerGraveyards; }
+
+    // Server functions
+    public String receiveCommand(String s) {
+        return "";
+    }
+
+    public String rollDice(String s) {
+        return "";
+    }
+
+    public String dealCards(String s) {
+        return "";
+    }
+
+    public String dealCard(String s) {
+        return "";
+    }
+
+    public String sendCard(String s) {
+        return "";
+    }
+
+    public String placeCard(String s) {
+        return "";
+    }
+
+    public String attackEnemyPlayer(String s) {
+        return "";
+    }
+
+    public String attackEnemyCreature(String s) {
+        return "";
+    }
+
+    public String healPlayer(String s) {
+        return "";
+    }
+
+    public String healCreature(String s) {
+        return "";
+    }
+
+    public String checkPlayerAlive(String s) {
+        return "";
+    }
+
+    public String checkCreatureAlive(String s) {
+        return "";
+    }
+
+    public String moveToGraveyard(String s) {
+        return "";
+    }
+
+    public String endTurn(String s) {
+        return "";
+    }
+
+    public String quitGame(String s) {
+        return "";
+    }
+
+}
