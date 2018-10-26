cef5d0236d989eef2c7defec6a592b29314fb4b4
Laurea <laurea.lindroos@hotmail.com>
2018-10-26 09:49:43 +0200

    Created BasicCreatureCard

---
+++ src/main/java/card/BasicCreatureCard.java
@@ -0,0 +1,43 @@
+package card;
+
+public class BasicCreatureCard extends BasicCard {
+    int health;
+    int attack;
+    int defence;
+
+
+
+    public BasicCreatureCard(int id, String name, String flavourText, String image, int health, int attack, int defence) {
+        super(id, name, flavourText, image);
+        this.health = health;
+        this.attack = attack;
+        this.defence = defence;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+
+
+}
