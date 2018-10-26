9425b1c0bd0bdd176e7dddec4d39c1555ad0d96e
Linn Mårtensson <linn.martensson@hotmail.se>
2018-10-26 12:02:28 +0200

    adjusted from static

--- src/main/java/repository/DummyDB.java
+++ src/main/java/repository/DummyDB.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DummyDB {
-   static List<BasicCreatureCard> database = Arrays.asList(
+   List<BasicCreatureCard> database = Arrays.asList(
         new BasicCreatureCard(1, "Marshmallow", "White soft treat", "does not exist yet", 3, 1, 2),
         new BasicCreatureCard(2, "Plopp","Chocolate with gooey caramel center", "does not excist", 2, 2, 1),
         new BasicCreatureCard(3, "Smash", "Crispy chocolate treat", "does not exist yet", 1, 5, 1),
