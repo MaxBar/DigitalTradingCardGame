35294810187bc128283dbae74671dbdde696a5db
Max Barnell <max.barnell@gmail.com>
2018-10-26 13:05:36 +0200

    Dummydata
    Added dummydata to game and player, changed DummyDB to use super and sub-class

--- src/main/java/repository/DummyDB.java
+++ src/main/java/repository/DummyDB.java
@@ -1,5 +1,6 @@
 package repository;
 
+import card.BasicCard;
 import card.BasicCreatureCard;
 
 
@@ -7,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DummyDB {
-   List<BasicCreatureCard> database = Arrays.asList(
+   public List<? extends BasicCard> database = Arrays.asList(
         new BasicCreatureCard(1, "Marshmallow", "White soft treat", "does not exist yet", 3, 1, 2),
         new BasicCreatureCard(2, "Plopp","Chocolate with gooey caramel center", "does not excist", 2, 2, 1),
         new BasicCreatureCard(3, "Smash", "Crispy chocolate treat", "does not exist yet", 1, 5, 1),
