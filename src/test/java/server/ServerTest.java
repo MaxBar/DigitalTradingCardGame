c9db210c4034c373ce715cd9f85369a5c9d07351
Unknown <arbenf@hotmail.se>
2018-10-29 09:50:20 +0100

    Created RollDice test

--- src/test/java/server/ServerTest.java
+++ src/test/java/server/ServerTest.java
@@ -3,6 +3,7 @@ package server;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerTest {
@@ -15,8 +16,13 @@ class ServerTest {
     void receiveCommand() {
     }
 
+    Server server = new Server();
+
     @Test
     void rollDice() {
+        for(int i = 0; i <= 10000; ++i) {
+            assertThat(server.rollDice(6,1)).isBetween(1, 6);
+        }
     }
 
     @Test
