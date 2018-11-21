5085ee16503edcaf0b312a3a3e94d3ad4b71eba4
Laurea <laurea.lindroos@hotmail.com>
2018-11-21 12:04:18 +0100

    ...

---
+++ src/main/java/main/Main2.java
@@ -0,0 +1,31 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main2 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent menu = FXMLLoader.load(getClass().getResource("../GUI/menu.fxml"));
+        primaryStage.setTitle("Candy Wars");
+        primaryStage.setScene(new Scene(menu, 1280, 720));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    public void renderGame(Stage gameStage) throws Exception {
+        Parent game = FXMLLoader.load(getClass().getResource("../GUI/game.fxml"));
+        gameStage.setTitle("Candy Wars");
+        gameStage.setScene(new Scene(game, 1280, 720));
+        gameStage.show();
+    }
+}
