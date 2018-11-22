ffdf230d998f2bd2d128438a7b6bb911b1d9bc67
Laurea <laurea.lindroos@hotmail.com>
2018-11-22 11:21:50 +0100

    Saving input in create account

--- src/main/java/Controllers/AccountController.java
+++ src/main/java/Controllers/AccountController.java
@@ -1,10 +1,15 @@
 package Controllers;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import main.Main2;
+
+
 
 public class AccountController {
     String name;
@@ -12,7 +17,15 @@ public class AccountController {
     String password;
 
     @FXML
+    private TextField accountName;
+    @FXML
+    private TextField accountEmail;
+    @FXML
+    private TextField accountPassword;
+
+    @FXML
     void handleSubmitButton() throws Exception{
+        saveEnteredDetails();
         renderMenu();
 
 }
@@ -22,4 +35,11 @@ public class AccountController {
         Main2.primaryStage.setScene(new Scene(game, 1280, 720));
         Main2.primaryStage.show();
     }
+    void saveEnteredDetails(){
+        name = accountName.getText();
+        email = accountEmail.getText();
+        password = accountPassword.getText();
+        System.out.println(name);
+    }
+
 }
