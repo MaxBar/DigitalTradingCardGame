34fa8dd537aad21b334f6054c6336d9944b6a805
Laurea <laurea.lindroos@hotmail.com>
2018-11-22 14:55:27 +0100

    Created Highscore Class with getters and setters

---
+++ src/main/java/main/Highscore.java
@@ -0,0 +1,58 @@
+package main;
+
+public class Highscore {
+
+    private String name;
+    private int wins;
+    private int draws;
+    private int losses;
+    private int points;
+
+    public Highscore(String name, int wins, int draws, int losses, int points) {
+        this.name = name;
+        this.wins = wins;
+        this.draws = draws;
+        this.losses = losses;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
