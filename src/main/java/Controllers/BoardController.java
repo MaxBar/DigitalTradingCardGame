41c1028ce0e6f0809885d093c528de645664b5ea
johan <johan_cool01@hotmail.com>
2018-11-22 13:31:34 +0100

    Move cards from hand to table

--- src/main/java/Controllers/BoardController.java
+++ src/main/java/Controllers/BoardController.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -22,10 +24,17 @@ import java.util.List;
 public class BoardController {
     //public Data data = new Data();
     AnchorPane cardPane;
-    public List<Card> newCard= Arrays.asList(
+    Button btn;
+    int choiceHandCard;
+    int choiceTableCard;
+    ArrayList<AnchorPane> hbox = new ArrayList<>();
+    public ArrayList<Card> newCard= new ArrayList<>( Arrays.asList(
             new Card(5, "Gröt", "/img/background.jpg"),
-            new Card(5, "Mullvad", "/img/candycard.jpg")
-    );
+            new Card(5, "Mullvad", "/img/candycard.jpg"),
+            new Card(5, "djungelvrål", "/img/candycard.jpg"),
+            new Card(5, "godis", "/img/candycard.jpg")
+    ));
+    public List<Card> tableCard= new ArrayList<>();
 
 
     @FXML HBox playerHand;
@@ -51,17 +60,46 @@ public class BoardController {
     }
 
     void doSomething() throws IOException {
-
+        playerHand.getChildren().clear();
+        //Draw Player Hand
         for (int i = 0; i < newCard.size() ; i++) {
+            btn = new Button(""+i);
+            btn.setPrefSize(190, 200);
+            btn.setOpacity(0);
+           // newCard.get(i).setId(i);
             cardPane = FXMLLoader.load(getClass().getResource("/card.fxml"));
             ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(newCard.get(i).getName());
             ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(newCard.get(i).getId()));
             ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(newCard.get(i).getUrl()));
-            int cardIndex= i;
+
+            cardPane.getChildren().add(btn);
+
+            hbox.add(cardPane);
+            //System.out.println(btn.getId());
 
 
-            playerHand.getChildren().add(cardPane);
+            int finalI = i;
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+
+                                public void handle(ActionEvent event) {
+                                    //System.out.println(finalI);
+                                    choiceHandCard = finalI;
+                                    tableCard.add(newCard.get(finalI));
+                                  //  System.out.println(tableCard.size());
+                                    newCard.remove(finalI);
+                                    System.out.println(tableCard.get(tableCard.size()-1).getName());
+                                    try {
+                                        drawTable();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }});
+
+            playerHand.getChildren().add(i,cardPane);
         }
+
+//        System.out.println(playerHand.getChildren().get(2));
         //Plats i HBoxen
         //  playerHand.getChildren().add(4,cardPane);
 
@@ -70,6 +108,42 @@ public class BoardController {
 
 
         // playerHand.getChildren().add();
+    }
+    //Draw Player Table
+    void drawTable() throws IOException{
+        playerTable.getChildren().clear();
+        doSomething();
+//        if(playerTable.getChildren() != null) {
+//            playerTable.getChildren().remove(0);
+//        }
+        for (int i = 0; i < tableCard.size(); i++) {
+            System.out.println("in tableCard");
+            btn = new Button("" + i);
+            btn.setPrefSize(190, 200);
+            btn.setOpacity(0);
+            // newCard.get(i).setId(i);
+            cardPane = FXMLLoader.load(getClass().getResource("/card.fxml"));
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(tableCard.get(i).getName());
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(tableCard.get(i).getId()));
+            ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(tableCard.get(i).getUrl()));
+
+            cardPane.getChildren().add(btn);
+
+            //hbox.add(cardPane);
+            //System.out.println(btn.getId());
+
+
+            int finalI = i;
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+
+                public void handle(ActionEvent event) {
+                    System.out.println(finalI);
+                    choiceTableCard = finalI;
+                }
+            });
+
+            playerTable.getChildren().add(i, cardPane);
+        }
     }
 
 }
