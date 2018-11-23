9e73db676c1f4c9aa8386fb318e91c2ee66fdbf6
johan <johan_cool01@hotmail.com>
2018-11-23 11:56:43 +0100

    Added so you can attack an enemy and it creates a string of 2 indexes so you see who you choose

--- src/main/java/Controllers/BoardController.java
+++ src/main/java/Controllers/BoardController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -15,7 +14,6 @@ import javafx.scene.layout.HBox;
 import main.Card;
 //import main.Data;
 
-import javax.sql.rowset.CachedRowSet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,20 +23,32 @@ public class BoardController {
     //public Data data = new Data();
     AnchorPane cardPane;
     Button btn;
-    int choiceHandCard;
-    int choiceTableCard;
+    int choiceEnemyCard;
+    int choicePlayerCard;
     ArrayList<AnchorPane> hbox = new ArrayList<>();
-    public ArrayList<Card> newCard= new ArrayList<>( Arrays.asList(
+    public ArrayList<Card> playerHandCards = new ArrayList<>( Arrays.asList(
             new Card(5, "Gröt", "/img/background.jpg"),
             new Card(5, "Mullvad", "/img/candycard.jpg"),
             new Card(5, "djungelvrål", "/img/candycard.jpg"),
             new Card(5, "godis", "/img/candycard.jpg")
     ));
-    public List<Card> tableCard= new ArrayList<>();
-
+    public ArrayList<Card> enemyHandCards= new ArrayList<>( Arrays.asList(
+            new Card(5, "Gröt", "/img/background.jpg"),
+            new Card(5, "Mullvad", "/img/candycard.jpg"),
+            new Card(5, "djungelvrål", "/img/candycard.jpg"),
+            new Card(5, "godis", "/img/candycard.jpg")
+    ));
+    public List<Card> playerTableCards = new ArrayList<>();
+    public List<Card> enemyTableCards = new ArrayList<>( Arrays.asList(
+            new Card(5, "Gröt", "/img/background.jpg"),
+            new Card(5, "Mullvad", "/img/candycard.jpg"),
+            new Card(5, "djungelvrål", "/img/candycard.jpg"),
+            new Card(5, "godis", "/img/candycard.jpg")
+    ));
 
     @FXML HBox playerHand;
     @FXML HBox playerTable;
+
     @FXML HBox enemyHand;
     @FXML HBox enemyTable;
     @FXML Button endTurnButton;
@@ -54,40 +64,41 @@ public class BoardController {
 
 
     public void initialize() throws IOException{
-        doSomething();
+        drawPlayerHandCards();
+        drawEnemyHand();
+        drawEnemyTable();
+
 
 
     }
 
-    void doSomething() throws IOException {
+    void drawPlayerHandCards() throws IOException {
         playerHand.getChildren().clear();
         //Draw Player Hand
-        for (int i = 0; i < newCard.size() ; i++) {
+        for (int i = 0; i < playerHandCards.size() ; i++) {
             btn = new Button(""+i);
             btn.setPrefSize(190, 200);
             btn.setOpacity(0);
-           // newCard.get(i).setId(i);
+           // playerHandCards.get(i).setId(i);
             cardPane = FXMLLoader.load(getClass().getResource("/card.fxml"));
-            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(newCard.get(i).getName());
-            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(newCard.get(i).getId()));
-            ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(newCard.get(i).getUrl()));
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(playerHandCards.get(i).getName());
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(playerHandCards.get(i).getId()));
+            ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(playerHandCards.get(i).getUrl()));
 
             cardPane.getChildren().add(btn);
 
             hbox.add(cardPane);
-            //System.out.println(btn.getId());
+
 
 
             int finalI = i;
             btn.setOnAction(new EventHandler<ActionEvent>() {
 
                                 public void handle(ActionEvent event) {
-                                    //System.out.println(finalI);
-                                    choiceHandCard = finalI;
-                                    tableCard.add(newCard.get(finalI));
-                                  //  System.out.println(tableCard.size());
-                                    newCard.remove(finalI);
-                                    System.out.println(tableCard.get(tableCard.size()-1).getName());
+
+                                    System.out.println(finalI);
+                                    playerTableCards.add(playerHandCards.get(finalI));
+                                    playerHandCards.remove(finalI);
                                     try {
                                         drawTable();
                                     } catch (IOException e) {
@@ -98,51 +109,90 @@ public class BoardController {
 
             playerHand.getChildren().add(i,cardPane);
         }
-
-//        System.out.println(playerHand.getChildren().get(2));
-        //Plats i HBoxen
-        //  playerHand.getChildren().add(4,cardPane);
-
-
-//        cardPane.getChildren().get()
-
-
-        // playerHand.getChildren().add();
     }
     //Draw Player Table
     void drawTable() throws IOException{
         playerTable.getChildren().clear();
-        doSomething();
-//        if(playerTable.getChildren() != null) {
-//            playerTable.getChildren().remove(0);
-//        }
-        for (int i = 0; i < tableCard.size(); i++) {
-            System.out.println("in tableCard");
+        drawPlayerHandCards();
+        for (int i = 0; i < playerTableCards.size(); i++) {
+
             btn = new Button("" + i);
             btn.setPrefSize(190, 200);
             btn.setOpacity(0);
-            // newCard.get(i).setId(i);
             cardPane = FXMLLoader.load(getClass().getResource("/card.fxml"));
-            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(tableCard.get(i).getName());
-            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(tableCard.get(i).getId()));
-            ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(tableCard.get(i).getUrl()));
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(playerTableCards.get(i).getName());
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(playerTableCards.get(i).getId()));
+            ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(playerTableCards.get(i).getUrl()));
 
             cardPane.getChildren().add(btn);
 
-            //hbox.add(cardPane);
-            //System.out.println(btn.getId());
 
 
             int finalI = i;
             btn.setOnAction(new EventHandler<ActionEvent>() {
 
                 public void handle(ActionEvent event) {
-                    System.out.println(finalI);
-                    choiceTableCard = finalI;
+                    choicePlayerCard = finalI;
                 }
             });
 
             playerTable.getChildren().add(i, cardPane);
+        }
+    }
+
+    void drawEnemyHand() throws IOException {
+        enemyHand.getChildren().clear();
+
+        //Draw Player Hand
+        for (int i = 0; i < enemyHandCards.size() ; i++) {
+            btn = new Button(""+i);
+            btn.setPrefSize(190, 200);
+            btn.setOpacity(0);
+            cardPane = FXMLLoader.load(getClass().getResource("/enemyCard.fxml"));
+            enemyHand.getChildren().add(i,cardPane);
+        }
+
+    }
+
+    void drawEnemyTable() throws IOException{
+        enemyTable.getChildren().clear();
+        drawPlayerHandCards();
+        for (int i = 0; i < enemyTableCards.size(); i++) {
+
+            btn = new Button("" + i);
+            btn.setPrefSize(190, 200);
+            btn.setOpacity(0);
+            cardPane = FXMLLoader.load(getClass().getResource("/card.fxml"));
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardName")))).setText(enemyTableCards.get(i).getName());
+            ((Label) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardManaCost")))).setText(String.valueOf(enemyTableCards.get(i).getId()));
+            ((ImageView) cardPane.getChildren().get(cardPane.getChildren().indexOf(cardPane.lookup("#cardImg")))).setImage(new Image(enemyTableCards.get(i).getUrl()));
+
+            cardPane.getChildren().add(btn);
+
+
+
+
+            int finalI = i;
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+
+                public void handle(ActionEvent event) {
+                        if(choicePlayerCard>-1) {
+                            choiceEnemyCard = finalI;
+                            System.out.printf("%s attacked %s OMFG \n", choicePlayerCard, choiceEnemyCard);
+
+                            choicePlayerCard = -10;
+                            choiceEnemyCard = -10;
+                        }
+
+                    try {
+                        drawTable();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }});
+
+            enemyTable.getChildren().add(i, cardPane);
         }
     }
 
